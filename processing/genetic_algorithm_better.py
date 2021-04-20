979972113d037ea6d6c4443907c647fcbcfc7e66
umachaba <the.bigbran.hypothesis@gmail.com>
2021-04-20 10:56:04 +0900

    add comments

--- processing/genetic_algorithm_better.py
+++ processing/genetic_algorithm_better.py
@@ -2,8 +2,6 @@ import random
 
 NUM_OF_CITIES = 100
 cities = []
-random_improvements = 0
-mutated_improvements = 0
 NUM_OF_POPULATION = 1000
 population = []
 
@@ -11,41 +9,54 @@ def setup():
     global cities, best_route, best_distance, first, population
     size(600, 600)
 
+    # generate random cities
     for i in range(NUM_OF_CITIES):
         city = City(random.randint(50, width - 50), random.randint(50, height - 50), i)
         cities.append(city)
+        
+    # add empty route to population list
     for i in range(NUM_OF_POPULATION):
         population.append(Route())
+        
+    # initiate best data
     best_route = random.choice(population)
     best_distance = best_route.calcLength()
     first = best_distance
     
 def draw():
+    # not displayed - todo
     textSize(20)
     fill(255)
     text('Best distance: ' + str(best_distance), 50, 50)
     
-    # global best_route, best_distance, random_improvements, mutated_improvements
     global best_route, best_distance, population
     background(0)
-    best_route.display()
-    # print(best_distance)
     
+    # display current best route
+    best_route.display()
+    
+    # sort population list by length
     population.sort(key = Route.calcLength)
-    population = population[:NUM_OF_POPULATION]
-    distance_1 = population[0].calcLength()
-    if distance_1 < best_distance:
-        best_distance = distance_1
+    population = population[:NUM_OF_POPULATION] # cut population list. list is enlarged because of the operation below
+    distance = population[0].calcLength() # total length of the first route in population list
+    # update best distance if first route length is shorter than it
+    if distance < best_distance:
+        best_distance = distance
         best_route = population[0]
         
+    # generate better routes from current population list
     for i in range(NUM_OF_POPULATION):
-        parentA, parentB = random.sample(population, 2)
-        child = parentA.crossover(parentB)
+        parentA, parentB = random.sample(population, 2) # pick random two parents (routes) from population list
+        child = parentA.crossover(parentB) # combine the parents
         population.append(child)
+    
+    # improve best route and append it to population
     for i in range(3, 25):
         if i < NUM_OF_CITIES:
             new = best_route.mutate(i)
             population.append(new)
+            
+    # improve random population and append it to population
     for i in range(3, 25):
         if i < NUM_OF_CITIES:
             new = random.choice(population)
@@ -79,16 +90,18 @@ class Route:
             cities[i].display()
         endShape(CLOSE)
         
+    # calculate total length of self
     def calcLength(self):
         self.distance = 0
         for i, num in enumerate(self.city_nums):
             self.distance += dist(cities[num].x, cities[num].y, cities[self.city_nums[i - 1]].x, cities[self.city_nums[i - 1]].y)
         return self.distance
     
+    # swap random city nums
     def mutate(self, num):
         indices = random.sample(list(range(NUM_OF_CITIES)), num)
         child = Route()
-        child.city_nums = self.city_nums[::]
+        child.city_nums = self.city_nums[::] # hard copy
         for i in range(num - 1):
             child.city_nums[indices[i]], child.city_nums[indices[(i + 1) % num]] = child.city_nums[indices[(i + 1) % num]], child.city_nums[indices[i]]
         return child
@@ -98,8 +111,8 @@ class Route:
         index = random.randint(1, NUM_OF_CITIES - 2)
         child.city_nums = self.city_nums[:index]
         if random.random() < 0.5:
-            child.city_nums = child.city_nums[::-1]
-        notinslice = [x for x in partner.city_nums if x not in child.city_nums]
+            child.city_nums = child.city_nums[::-1] # invert at 50 percent chance
+        notinslice = [x for x in partner.city_nums if x not in child.city_nums] # partner cities which is not overlapped with self cities
         
         child.city_nums += notinslice
         return child
